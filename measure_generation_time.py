Fix bug - allow optional attention mask in BlockTransformer.generate
Use generate directly instead of using preprocess_inputs_for_generation in measure_generation_time.py.

Note that previous code required the caller to call `preprocess_inputs_for_generation` to convert vanilla format inputs ([bs, seq_length]) to block format ([bs, num_blocks, block_length]) before calling `BlockTransformer.generate`, but now you can just pass vanilla-format inputs to `BlockTransformer.generate`.

measure_generation_time.py
@@ -139,24 +139,23 @@ def generate_with_block_model_and_measure_time(model: BlockTransformer, prefill_
     """
     max_length = get_max_length(model.block_length, prefill_length, decode_length)
     input_ids = torch.randint(0, model.config.vocab_size, (batch_size, prefill_length))
-    inputs = model.preprocess_inputs_for_generation(input_ids.to(DEVICE))
 
     start = torch.cuda.Event(enable_timing=True)
     end = torch.cuda.Event(enable_timing=True)
 
     start.record()
     if log_path is None:
-        output = model.generate(**inputs, max_length=max_length, benchmark=False)
+        output = model.generate(input_ids.to(DEVICE), max_length=max_length, benchmark=False)
     else:
         with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA],
                      record_shapes=False, profile_memory=True, with_stack=False) as prof:
             with record_function("model_inference"):
-                output = model.generate(**inputs, max_length=max_length, benchmark=False)
+                output = model.generate(input_ids.to(DEVICE), max_length=max_length, benchmark=False)
         print(prof.key_averages().table(sort_by="cuda_time_total", row_limit=10))
         prof.export_chrome_trace(log_path)
     end.record()
 
-    assert output.shape[1] * output.shape[2] == math.ceil(max_length / model.block_length) * model.block_length
+    assert output.shape[1] == max_length or print(output.shape[1])
     torch.cuda.synchronize()
 
     return start.elapsed_time(end)

model/block_transformer.py
@@ -224,8 +224,13 @@ class BlockTransformer(nn.Module):
             "auto_encoding_loss": auto_encoding_loss,
         }
 
-    def preprocess_inputs_for_generation(self, input_ids: torch.LongTensor, attention_mask: torch.LongTensor):
+    def preprocess_inputs_for_generation(self,
+                                         input_ids: torch.LongTensor,
+                                         attention_mask: Optional[torch.LongTensor] = None):
         """
+        Convert vanilla format generation inputs to block format. Note that this is called automatically by
+        BlockTranformer.generate.
+
         :return: {
             "input_ids": torch.Tensor[batch_size, n_blocks, block_length],
             "attention_mask": torch.Tensor[batch_size, n_blocks, block_length],
@@ -233,7 +238,11 @@ class BlockTransformer(nn.Module):
         }
         """
         input_ids = input_ids.view(-1, input_ids.shape[-1])
-        attention_mask = attention_mask.view(-1, attention_mask.shape[-1])
+        if attention_mask is None:
+            attention_mask = input_ids.new_ones(input_ids.shape, dtype=torch.long)
+            attention_mask[input_ids == self.embedder.config.pad_token_id] = 0
+        else:
+            attention_mask = attention_mask.view(-1, attention_mask.shape[-1])
         bs, length = input_ids.shape
 
         block_boundary_remainder = input_ids.shape[-1] % self.block_length
@@ -301,7 +310,7 @@ class BlockTransformer(nn.Module):
             "attention_mask": attention_mask.shape if attention_mask is not None else None,
             "block_attention_mask": block_attention_mask.shape if block_attention_mask is not None else None,
         }
-        vanilla_mode = block_attention_mask is None
+        vanilla_mode = block_attention_mask is None  # inputs are in vanilla transformer.generate format
         if vanilla_mode:  # vanilla format
             d = self.preprocess_inputs_for_generation(input_ids, attention_mask)
             input_ids = d["input_ids"]